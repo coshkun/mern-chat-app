57667d36ef2022edee785f7b868557e1d5b43b35
Coskun GOZEN (GSS) <coskun.caner@gozendigital.com>
2024-06-24 15:00:45 +0300

    2nd part completed - ui design

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div className="chat chat-end">
+        <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+                <img
+                    alt="Tailwind CSS chat bubble component"
+                    src="https://avatar.iran.liara.run/public/45"/>
+            </div>
+        </div>
+        {/* <div className="chat-header">
+            Obi-Wan Kenobi
+            <time className="text-xs opacity-50"> 12:45</time>
+        </div> */}
+        <div className="chat-bubble text-white bg-blue-500">You were the Chosen One!</div>
+        {/* <div className="chat-footer opacity-50">Delivered</div> */}
+        <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">12:48</div> {/* ⎷⎷  */}
+    </div>
+  )
+}
+
+export default Message
---
+++ frontend/src/pages/signup/GenderCheckBox.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const GenderCheckBox = () => {
+  return (
+    <div className='flex mt-2'>
+        <div className=' form-control'>
+            <label className='label gap-2 cursor-pointer' >
+                <span className=' label-text'>Male</span>
+                <input type="checkbox" className=' checkbox border-slate-900'/>
+            </label>
+        </div>
+        <div>
+            <label className='label gap-2 cursor-pointer' >
+                <span className=' label-text'>Female</span>
+                <input type="checkbox" className=' checkbox border-slate-900'/>
+            </label>
+        </div>
+    </div>
+  )
+}
+
+export default GenderCheckBox
+
+
+
+// STARTER CODE OF THIS FILE:
+// const GenderCheckBox = () => {
+//     return (
+//       <div className='flex mt-2'>
+//           <div className=' form-control'>
+//               <label className='label gap-2 cursor-pointer' >
+//                   <span className=' label-text'>Male</span>
+//                   <input type="checkbox" className=' checkbox border-slate-900'/>
+//               </label>
+//           </div>
+//           <div>
+//               <label className='label gap-2 cursor-pointer' >
+//                   <span className=' label-text'>Female</span>
+//                   <input type="checkbox" className=' checkbox border-slate-900'/>
+//               </label>
+//           </div>
+//       </div>
+//     )
+//   }
